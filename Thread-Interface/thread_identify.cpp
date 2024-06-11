cbce853392310048efb1289991620e71686c6c19
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-06-11 17:48:27 +0530

    Thread id, native_handle and std::chrono/ std::literals

---
+++ Thread-Interface/thread_identify.cpp
@@ -0,0 +1,70 @@
+#include <thread>
+#include <iostream>
+
+class ThreadRunner{
+    public:
+        void operator()() {
+            std::cout << "ThreadRunner operator() was the entry point." << std::endl;
+        }
+        void classMethodEntryPoint() {
+            std::cout << "ThreadRunner classMethodEntryPoint was the entry point." << std::endl;
+        }
+        void sleepThreadEntryPoint() {
+            using namespace std::literals;
+            std::cout << "ThreadRunner sleepThreadEntryPoint was the entry point." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            std::cout << "ThreadRunner sleepThreadEntryPoint woke up after 5 seconds" << std::endl;
+            std::this_thread::sleep_for(2s);
+            std::cout << "ThreadRunner sleepThreadEntryPoint woke up after 2 seconds." << std::endl;
+        }
+        ThreadRunner() {};
+        ~ThreadRunner() = default;
+};
+
+int main() {
+
+    {
+        /**
+         * Native handle is a unique identifier for the thread. It is a system specific identifier.
+         * It is not portable, and is set to null after the thread is destroyed. 
+         * So a null handle means that the thread is not running.
+         */
+        ThreadRunner tr;
+        std::thread thr(tr);
+        std::cout << "Thread native handle (Before): " << thr.native_handle() << std::endl;
+        thr.join();
+        std::cout << "Thread native handle (After): " << thr.native_handle() << std::endl;
+    }
+
+    {
+        /**
+         * std::thread::get_id returns a unique identifier for the thread. It is a system specific identifier.
+         * Note that once a thread dies, the id can be reused by another thread.
+         */
+        std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+        ThreadRunner tr;
+        std::thread thr(&ThreadRunner::classMethodEntryPoint, &tr);
+        std::thread thr2(tr);
+        std::cout << "thr id: (Before) " << thr.get_id() << std::endl;
+        std::cout << "thr2 id: (Before) " << thr2.get_id() << std::endl;
+        thr.join();
+        thr2.join();
+        std::cout << "thr id: (After) " << thr.get_id() << std::endl;
+        std::cout << "thr2 id: (After) " << thr2.get_id() << std::endl;
+    }
+
+    {
+
+        /**
+         * std::chrono is a library that provides time related functions.
+         * std::literals is a namespace that we can use to craete literals for time related functions,
+         * eg 2s == 2 seconds.
+         */
+        ThreadRunner thr;
+        std::thread t1(&ThreadRunner::sleepThreadEntryPoint, std::ref(thr));
+        std::thread t2(thr);
+        t1.join();
+        t2.join();
+    }
+    return EXIT_SUCCESS;
+}
